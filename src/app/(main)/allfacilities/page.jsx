296e94dfd3e9f40dd61408242b1ab4ada8ce6f0d
Muntasir333 <mdfahimmuntasir333@gmail.com>
2026-05-26 16:40:39 +0600

    10th commit

--- src/app/(main)/allfacilities/page.jsx
+++ src/app/(main)/allfacilities/page.jsx
@@ -16,7 +16,7 @@ const Allfacilities = async () => {
                         <h2>{facility.facilityName}</h2>
                         <p>{facility.description}</p>
                         <a href={`/allfacilities/${facility._id}`}>
-                            <button className='bg-blue-500 text-white px-4 py-2 rounded'>Book Now</button>
+                            <button className='bg-blue-500 text-white px-4 py-2 rounded'>See Details</button>
                         </a>   
                                
                     </li>
